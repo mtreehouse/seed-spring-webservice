6c63320a9084b56153d50556c36407f3763d5779
Yunwoo Kim <sym3927@naver.com>
2020-01-01 01:54:07 +0900

    idle

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -4,11 +4,11 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 function switch_proxy(){
-  IDEL_PORT=$(find_idle_port)
+  IDLE_PORT=$(find_idle_port)
 
-  echo "> 전환할 port: $IDEL_PORT"
+  echo "> 전환할 port: $IDLE_PORT"
   echo "> Port 전환"
-  echo 'set \$service_url http://127.0.0.1:${IDEL_PORT};' | sudo tee /etc/nginx/conf.d/service-url.inc
+  echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
   echo "> 엔진엑스 Reload"
   sudo service nginx reload
